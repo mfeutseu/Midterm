87685bf96e59e94781b835e9c399a31d91f36fcb
mfeutseu <mfeutseu@gmail.com>
2022-10-28 22:26:40 -0400

    updated config to point on Mongo Atlas

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
-export const MongoURI = "mongodb://localhost:27017/media";
-//export const MongoURI = "mongodb+srv://mfeutseu:Korrea01!@cluster0.2cm3xni.mongodb.net/books?retryWrites=true&w=majority";
+//export const MongoURI = "mongodb://localhost:27017/media";
+export const MongoURI = "mongodb+srv://mfeutseu:Korrea01!@cluster0.2cm3xni.mongodb.net/books?retryWrites=true&w=majority";
 
 
 export const Secret = "someSecret";
